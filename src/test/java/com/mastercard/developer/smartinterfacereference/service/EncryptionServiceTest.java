4b822044997fd189a307b99e958eb6fb494f9267
Mabry, Zachary <zachary.mabry@mastercard.com>
2022-01-19 11:29:47 -0600

    additional tests for encryptionService

--- src/test/java/com/mastercard/developer/smartinterfacereference/service/EncryptionServiceTest.java
+++ src/test/java/com/mastercard/developer/smartinterfacereference/service/EncryptionServiceTest.java
@@ -1,24 +1,55 @@
 package com.mastercard.developer.smartinterfacereference.service;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mastercard.developer.smartinterfacereference.service.EncryptionService.EncryptionException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class EncryptionServiceTest {
 
     private EncryptionService encryptionService;
+    private ObjectMapper mapper;
 
     @BeforeEach
     void setUp() {
-        ObjectMapper mapper = new ObjectMapper();
-        encryptionService = new EncryptionService("encryption/enc.unittests.crt", true, mapper);
-        encryptionService.init();
+        mapper = new ObjectMapper();
     }
 
     @Test
-    void encryptData() throws Exception {
+    void encryptDataEncryptionDisabled() {
+        encryptionService = new EncryptionService("encryption/enc.unittests.crt", false, mapper);
+        encryptionService.init();
+
+        assertThrows(EncryptionException.class, () -> {
+            encryptionService.encryptData("{\"test\":\"value\"}");
+        });
+    }
+
+    @Test
+    void encryptData() {
+        encryptionService = new EncryptionService("encryption/enc.unittests.crt", true, mapper);
+        encryptionService.init();
         String encryptedData = encryptionService.encryptData("{\"test\":\"value\"}");
         assertNotNull(encryptedData);
+    }
+
+    @Test
+    void encryptDataInvalidCert() {
+        encryptionService = new EncryptionService("invalid", true, mapper);
+        assertThrows(EncryptionException.class, () -> {
+            encryptionService.init();
+        });
+    }
+
+    @Test
+    void encryptDataInvalidData() {
+        encryptionService = new EncryptionService("encryption/enc.unittests.crt", true, mapper);
+        encryptionService.init();
+
+        assertThrows(EncryptionException.class, () -> {
+            encryptionService.encryptData("invalid");
+        });
     }
 }
